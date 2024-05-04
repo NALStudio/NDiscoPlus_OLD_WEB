529a4c58cde976b1c07d41074e48534133810045
Niko Leinonen <niko.a.leinonen@gmail.com>
2024-05-04 19:24:58 +0300

    asdf

--- lib/env/_debug.dart
+++
@@ -1,9 +0,0 @@
-import './env.dart';
-
-class DebugEnvironment extends Environment {
-  const DebugEnvironment();
-
-  @override
-  // TODO: implement spotifyClientId
-  String get spotifyClientId => throw UnimplementedError();
-}
--- lib/env/_release.dart
+++
@@ -1,9 +0,0 @@
-import './env.dart';
-
-class ReleaseEnvironment extends Environment {
-  const ReleaseEnvironment();
-
-  @override
-  // TODO: implement spotifyClientId
-  String get spotifyClientId => throw UnimplementedError();
-}
