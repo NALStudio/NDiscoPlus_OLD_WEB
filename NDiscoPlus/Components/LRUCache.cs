57dd4c95b234a76ffbb1e0394d015664a4d5524c
Niko Leinonen <niko.a.leinonen@gmail.com>
2024-09-10 22:25:15 +0300

    Fix LRU Cache

--- NDiscoPlus/Components/LRUCache.cs
+++ NDiscoPlus/Components/LRUCache.cs
@@ -1,80 +1,75 @@
-﻿using System.Collections;
+﻿using MaterialColorUtilities.Schemes;
+using System.Collections;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 
 namespace NDiscoPlus.Components;
 
-/// <summary>
-/// An LRU Cache implementation that does a linear search through it's values.
-/// Cache hit speed depends on how recently the data was accessed.
-/// Cache miss speed depends on the capacity of cache.
-/// </summary>
+// Reference: https://www.baeldung.com/java-lru-cache
 public class LRUCache<TKey, TValue> where TKey : IEquatable<TKey>
 {
-    private int offset;
-    private int count;
-    private readonly TKey[] keys;
-    private readonly TValue[] values;
+    private readonly record struct CacheEntry(TKey Key, TValue Value);
 
-    public int Count => count;
-    public int Capacity => keys.Length;
+    // LinkedList.Count is an O(1) operation
+    public int Count => linkedList.Count;
+    public int Capacity { get; }
 
-    // Use byte to limit capacity to max 255
-    // We want the capacity to be low as we do linear searches through the object
-    public LRUCache(byte capacity)
+    private readonly Dictionary<TKey, LinkedListNode<CacheEntry>> nodeMap;
+    private readonly LinkedList<CacheEntry> linkedList;
+
+    public LRUCache(int capacity)
     {
-        offset = 0;
-        count = 0;
-
-        keys = new TKey[capacity];
-        values = new TValue[capacity];
+        Capacity = capacity;
+        nodeMap = new(capacity: capacity);
+        linkedList = new();
     }
 
     public TValue GetOrAdd(TKey key, Func<TKey, TValue> valueBuilder)
     {
-        if (TryGet(key, out TValue? value))
-            return value;
-
-        TValue newValue = valueBuilder(key);
-        Add(key, newValue);
-        return newValue;
-    }
-
-    private bool TryGet(TKey key, [MaybeNullWhen(false)] out TValue value)
-    {
-        Debug.Assert(count <= Capacity);
-        for (int i = 0; i < count; i++)
+        if (nodeMap.TryGetValue(key, out LinkedListNode<CacheEntry>? node))
         {
-            // iterate backwards from the most recently used item
-            // most recently item is at index (offset - 1)
-            // this is done so that the the items that are accessed most often can be fetched first
-            // On cache:
-            //   - hit: should be faster than a dictionary lookup
-            //   - miss: will be slower depending on cache capacity
-            int index = (offset - 1) - i;
-            if (index < 0) // use if case instead since modulo returns a negative value
-                index += keys.Length;
+            // Node exists
 
-            if (key.Equals(keys[index]))
-            {
-                // value found
-                value = values[index];
-                return true;
-            }
+            MoveNodeToFirst(node);
+
+            return node.Value.Value;
         }
+        else
+        {
+            // Node doesn't exist
 
-        // value not found
-        value = default;
-        return false;
+            TValue newValue = valueBuilder(key);
+            CacheEntry newEntry = new(key, newValue);
+            LinkedListNode<CacheEntry> newNode = new(newEntry);
+            AddNewNode(newNode);
+
+            return newValue;
+        }
     }
 
-    private void Add(TKey key, TValue value)
+    private void MoveNodeToFirst(LinkedListNode<CacheEntry> node)
     {
-        keys[offset] = key;
-        values[offset] = value;
+        linkedList.Remove(node);
+        linkedList.AddFirst(node);
+    }
 
-        offset++;
-        if (count < Capacity)
-            count++;
+    private void AddNewNode(LinkedListNode<CacheEntry> newNode)
+    {
+        // Remove oldest entries until one free slot is available for the new value
+        while (Count >= Capacity)
+            RemoveOldestEntry();
+
+        // add to nodeMap first so that if an exception is thrown (key exists already)
+        // nodeMap and linkedList don't go out-of-sync.
+        nodeMap.Add(newNode.Value.Key, newNode);
+        linkedList.AddFirst(newNode);
+    }
+
+    // Remove least recently used item
+    private void RemoveOldestEntry()
+    {
+        LinkedListNode<CacheEntry>? node = linkedList.Last ?? throw new InvalidOperationException("Linked list is empty.");
+        linkedList.Remove(node);
+        nodeMap.Remove(node.Value.Key);
     }
 }
