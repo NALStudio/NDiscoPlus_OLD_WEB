bd22976c3852c95c5de59300fb861d3413deab54
Niko Leinonen <niko.a.leinonen@gmail.com>
2024-09-27 02:39:33 +0300

    Buggy new gradient canvas implementation

---
+++ NDiscoPlus/Components/GradientCanvas.razor.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using MudBlazor;
+using NDiscoPlus.Shared.Models.Color;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.InteropServices.JavaScript;
+using System.Runtime.Versioning;
+using System.Threading;
+
+namespace NDiscoPlus.Components;
+
+public partial class GradientCanvas : IAsyncDisposable
+{
+    private readonly record struct ShaderArgs(int ColorCount, bool UseHDR, bool Dither)
+    {
+        public Dictionary<string, string> ToArgumentDictionary()
+        {
+            static string BoolToGlslConstant(bool value) => value ? "true" : "false";
+            static string IntToGlslConstant(int value) => value.ToString(CultureInfo.InvariantCulture);
+
+            (int colorCount, bool useHdr, bool dither) = this;
+
+            if (colorCount != 4 && colorCount != 6)
+                throw new InvalidOperationException("Invalid color count.");
+
+            return new()
+            {
+                { "%LIGHT_COUNT%", IntToGlslConstant(colorCount) },
+                { "%USE_HDR%", BoolToGlslConstant(useHdr) },
+                { "%DITHERED%", BoolToGlslConstant(dither) },
+            };
+        }
+    }
+
+    private readonly record struct SizeArgs(int Width, int Height);
+
+    /// <summary>
+    /// Either 4 or 6 colors to render.
+    /// </summary>
+    /// <remarks>
+    /// Do not change the color count often as it forces a rebuild of the entire shader pipeline.
+    /// </remarks>
+    [Parameter, EditorRequired]
+    public IReadOnlyList<NDPColor>? Colors { get; set; }
+
+    [Parameter, EditorRequired]
+    public int Width { get; set; }
+
+    [Parameter, EditorRequired]
+    public int Height { get; set; }
+
+    [Parameter]
+    public bool UseHDR { get; set; } = false;
+    [Parameter]
+    public bool Dither { get; set; } = true;
+
+    [Parameter]
+    public string? Style { get; set; }
+
+    protected ElementReference? ParentDivReference { get; set; }
+
+    private ShaderArgs? previousShaderArgs;
+    private SizeArgs? previousSizeArgs;
+
+    private Task<IJSObjectReference>? program;
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+        RecreateProgramIfNeeded();
+    }
+
+    private void RecreateProgramIfNeeded()
+    {
+        Debug.Assert(Colors is not null);
+
+        ShaderArgs shaderArgs = new(Colors.Count, UseHDR, Dither);
+        SizeArgs sizeArgs = new(Width, Height);
+
+        if (program is null || shaderArgs != previousShaderArgs || sizeArgs != previousSizeArgs)
+        {
+            program = CreateProgram(
+                previousProgram: program,
+                shaderArgs: shaderArgs,
+                sizeArgs: sizeArgs,
+                colors: Colors
+            );
+            program.ContinueWith(_ => StateHasChanged());
+
+            previousShaderArgs = shaderArgs;
+            previousSizeArgs = sizeArgs;
+        }
+    }
+
+    private async Task<IJSObjectReference> CreateProgram(Task<IJSObjectReference>? previousProgram, ShaderArgs shaderArgs, SizeArgs sizeArgs, IReadOnlyList<NDPColor> colors)
+    {
+        if (previousProgram is not null)
+            await DisposeProgram(previousProgram);
+
+        IJSObjectReference module = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./Components/GradientCanvas.razor.js");
+
+        IJSObjectReference program = await module.InvokeAsync<IJSObjectReference>("createShaderPipeline", ParentDivReference, sizeArgs.Width, sizeArgs.Height, shaderArgs.UseHDR, shaderArgs.ToArgumentDictionary());
+        await program.InvokeVoidAsync("start_render", UnpackColors(colors));
+
+        return program;
+    }
+
+    protected override async Task OnParametersSetAsync()
+    {
+        Debug.Assert(Colors is not null);
+
+        if (program?.IsCompleted == true)
+            await program.Result.InvokeVoidAsync("set_colors", UnpackColors(Colors));
+    }
+
+    private static IEnumerable<double> UnpackColors(IEnumerable<NDPColor> colors)
+    {
+        foreach (NDPColor color in colors)
+        {
+            yield return color.X;
+            yield return color.Y;
+            yield return color.Brightness;
+        }
+    }
+
+    internal static async ValueTask DisposeProgram(Task<IJSObjectReference> program)
+    {
+        IJSObjectReference p = await program;
+        await p.InvokeVoidAsync("dispose");
+        await p.DisposeAsync();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (program is not null)
+            await DisposeProgram(program);
+    }
+}
---
+++ NDiscoPlus/Components/GradientCanvas.razor.js
@@ -0,0 +1,145 @@
+﻿// Heavy inspiration: https://adammurray.blog/webgl/tutorials/
+
+const vertices = [
+    [-1, -1],
+    [1, -1],
+    [-1, 1],
+    [1, 1],
+];
+const vertexData = new Float32Array(vertices.flat());
+
+/**
+ * @param {HTMLElement} divElementReference
+ * @param {number} width 
+ * @param {number} height
+ * @param {bool} useHDR
+ * @param {Map<String, String>} fragmentShaderArgs 
+ */
+export function createShaderPipeline(divElementReference, width, height, useHDR, fragmentShaderArgs) {
+    const canvas = divElementReference.querySelector("#GradientCanvas_canvas");
+    const vertexShaderSource = divElementReference.querySelector("#GradientCanvas_vertex").textContent;
+
+    let fragmentShaderSourceBuilder = divElementReference.querySelector("#GradientCanvas_fragment").textContent;
+    for (const fragmentArg of Object.entries(fragmentShaderArgs)) {
+        fragmentShaderSourceBuilder = fragmentShaderSourceBuilder.replace(fragmentArg[0], fragmentArg[1]);
+    }
+    const fragmentShaderSource = fragmentShaderSourceBuilder;
+
+    return createProgram(canvas, width, height, useHDR, vertexShaderSource, fragmentShaderSource);
+}
+
+/**
+ * @param {HTMLCanvasElement} canvas
+ * @param {number} width 
+ * @param {number} height 
+ * @param {String} vertexCode
+ * @param {String} fragmentCode
+ */
+function createProgram(canvas, width, height, useHDR, vertexCode, fragmentCode) {
+    const gl = canvas.getContext("webgl2");
+    if (!gl) throw "WebGL2 not supported";
+
+    gl.viewport(0, 0, width, height);
+    if (useHDR) {
+        throw "Not implemented.";
+    }
+
+    function createShader(shaderType, sourceCode) {
+        const shader = gl.createShader(shaderType);
+        gl.shaderSource(shader, sourceCode.trim());
+        gl.compileShader(shader);
+        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+            throw gl.getShaderInfoLog(shader);
+        }
+        return shader;
+    }
+
+    const program = gl.createProgram();
+    gl.attachShader(program, createShader(gl.VERTEX_SHADER, vertexCode));
+    gl.attachShader(program, createShader(gl.FRAGMENT_SHADER, fragmentCode));
+    gl.linkProgram(program);
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        throw gl.getProgramInfoLog(program);
+    }
+    gl.useProgram(program);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+    gl.bufferData(gl.ARRAY_BUFFER, vertexData, gl.STATIC_DRAW);
+
+    const vertexPosition = gl.getAttribLocation(program, "vertexPosition");
+    gl.enableVertexAttribArray(vertexPosition);
+    gl.vertexAttribPointer(vertexPosition, 2, gl.FLOAT, false, 0, 0);
+
+    const canvasSizeUniform = gl.getUniformLocation(program, "canvasSize");
+    gl.uniform2f(canvasSizeUniform, canvas.width, canvas.height);
+
+    return new WebGL2Program(gl, program);
+}
+
+/**
+ * @param {WebGL2Program} p
+ */
+function renderProgram(p) {
+    if (p.colors === null) throw "No colors set.";
+
+    p.gl.uniform3fv(p.lightColorsUnion, p.colors);
+    p.gl.drawArrays(p.gl.TRIANGLE_STRIP, 0, vertices.length);
+
+    if (p.rendering) {
+        requestAnimationFrame(() => renderProgram(p));
+    }
+}
+
+class WebGL2Program {
+    /**@type {bool}*/
+    rendering;
+
+    /**@type {?number[]}*/
+    colors;
+
+    /**
+     * @param {WebGL2RenderingContext} gl
+     * @param {WebGL2Program} program
+     */
+    constructor(gl, program) {
+        this.gl = gl;
+        this.program = program;
+
+        this.lightColorsUnion = gl.getUniformLocation(program, "lightColors");
+
+        this.rendering = false;
+        this.colors = null;
+    }
+
+    /**
+     * 
+     * @param {number[]} colors
+     */
+    set_colors(colors) {
+        this.colors = colors;
+    }
+
+    /**
+     * 
+     * @param {number[]} colors
+     */
+    start_render(colors) {
+        this.colors = colors;
+
+        if (!this.rendering) {
+            this.rendering = true;
+            renderProgram(this);
+        }
+    }
+
+    stop_render() {
+        if (this.rendering) {
+            this.rendering = false;
+        }
+    }
+
+    dispose() {
+        this.stop_render();
+        this.gl.deleteProgram(this.program);
+    }
+}
